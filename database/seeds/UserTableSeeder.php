443a1b3f4a7275dd18629d9e877e19219e29abbc
beeglebug <updownleftright@gmail.com>
2015-03-15 18:54:00 +0000

    initial commit

---
+++ database/seeds/UserTableSeeder.php
@@ -0,0 +1,21 @@
+<?php
+
+use App\User;
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Hash;
+
+class UserTableSeeder extends Seeder
+{
+    public function run()
+    {
+        DB::table('users')->delete();
+
+        User::create([
+            'email' => 'jezza126@gmail.com',
+            'name' => 'Jeremy',
+            'password' => Hash::make('password')
+        ]);
+    }
+
+}
